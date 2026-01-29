bd3ce76f302a3a388353fa984e15d0c67020609d
kabinSherma <kabinshermalimbu@gmail.com>
2026-01-29 18:43:13 +0545

    UI of home, card and navbar

--- Libary/src/Pages/Home.jsx
+++ Libary/src/Pages/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../GlobalComponents/Layout/Layout'
-
+import Cards from './Components/Cards'
+import axios from 'axios'
 const Home = () => {
+   const [book,setBook]=useState([])
+
+  const fetchBook =async()=>{
+    const response = await axios.get("http://localhost:3000/book/")
+    if(response == 200){
+      console.log("Book Fetched")
+    }
+  }
+
+  useEffect(()=>{
+    fetchBook()
+  },[])
+
   return (
     <Layout>
 
-      <h1>Home</h1>
+      <div className='m-25 flex justify-evenly flex-wrap'>
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        
+      </div>
 
 
     </Layout>
